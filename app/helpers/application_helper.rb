3cabe50cc1096652339a7bc27b14af3212afbf04
Keith Bennett <keithrbennett@gmail.com>
2020-05-03 20:31:15 -0400

    Add text re: passing away. Move definition of external_link method to ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def external_link(text, url)
+    tag.a(text, href:url, target: '_blank').html_safe
+  end
+
+  def li_external_link(text, url)
+    tag.li(external_link(text,url)).html_safe
+  end
+
 end
