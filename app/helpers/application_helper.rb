9f431e255c222546f92188abdef4b8ed21fe6e08
Keith Bennett <keithrbennett@gmail.com>
2020-05-05 21:31:06 -0400

    Fix italicized links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def external_link(text, url)
-    tag.a(text, href:url, target: '_blank').html_safe
+    tag.a(text.html_safe, href:url, target: '_blank').html_safe
   end
 
   def li_external_link(text, url)
-    tag.li(external_link(text,url)).html_safe
+    tag.li(external_link(text.html_safe, url)).html_safe
   end
 
 end
