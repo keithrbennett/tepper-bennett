c2df7d575578196ac4c4c30a3acd6db4b1faffc3
Keith Bennett <keithrbennett@gmail.com>
2020-05-07 01:09:56 -0400

    Add some songs.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -35,6 +35,61 @@ class StaticPagesController < ActionController::Base
   def init_recordings
     @recordings = [
         Recording.new(
+            'Red Roses for a Blue Lady',
+            'Andy Williams',
+            'https://www.youtube.com/watch?v=HssRO5b_ED0'
+        ),
+        Recording.new(
+            'Red Roses for a Blue Lady',
+            'Bert Kaempfert and His Orchestra',
+            'https://www.youtube.com/watch?v=zt6WdnrAvpE'
+        ),
+        Recording.new(
+            'Red Roses for a Blue Lady',
+            'Dean Martin',
+            'https://www.youtube.com/watch?v=drU6kuih41w'
+        ),
+        Recording.new(
+            'Red Roses for a Blue Lady',
+            'Frank Sinatra',
+            'https://www.youtube.com/watch?v=yeMFw9tBrGo'
+        ),
+        Recording.new(
+            'Red Roses for a Blue Lady',
+            'Guy Lombardo And His Royal Canadians',
+            'https://www.youtube.com/watch?v=_41H2cpTzNc'
+        ),
+        Recording.new(
+            'The Young Ones',
+            'Cliff Richard',
+            'https://www.youtube.com/watch?v=BxNohANhJiA'
+        ),
+        Recording.new(
+            'The Naughty Lady of Shady Lane',
+            'The Ames Brothers',
+            'https://youtu.be/9HxB7lxbTnI'
+        ),
+        Recording.new(
+            'Kiss of Fire',
+            'Louis Armstrong',
+            'https://youtu.be/gVxwN3Eaf_U'
+        ),
+        Recording.new(
+            "Nuttin' for Christmas",
+            'Barry Gordon',
+            'https://youtu.be/aUA7BPnog_0'
+        ),
+        Recording.new(
+            'Suzy Snowflake',
+            'Rosemary Clooney',
+            'https://www.youtube.com/watch?v=UiFXZhU5kp4'
+        ),
+        Recording.new(
+            'Wonderful World of the Young',
+            'Andy Williams',
+            'https://www.youtube.com/watch?v=eoRVnPH8uUI'
+        ),
+        Recording.new(
             'Bagel & Lox',
             'Rob Schneider',
             'https://www.youtube.com/watch?v=dv4h8yU_N7o'
@@ -50,49 +105,14 @@ class StaticPagesController < ActionController::Base
             'https://www.youtube.com/watch?v=0_Gycn0UJ9M'
         ),
         Recording.new(
-            'Kiss of Fire',
-            'Louis Armstrong',
-            'https://youtu.be/gVxwN3Eaf_U'
-        ),
-        Recording.new(
-            "Nuttin' for Christmas",
-            'Barry Gordon',
-            'https://youtu.be/aUA7BPnog_0'
-        ),
-        Recording.new(
-            'Red Roses for a Blue Lady',
-            'Bert Kaempfert and His Orchestra',
-            'https://www.youtube.com/watch?v=zt6WdnrAvpE'
-        ),
-        Recording.new(
-            'Red Roses for a Blue Lady',
-            'Dean Martin',
-            'https://www.youtube.com/watch?v=drU6kuih41w'
-        ),
-        Recording.new(
-            'Santa Claus Looks Just Like Daddy',
-            'Art Mooney & His Orchestra with Barry Gordon',
-            'https://www.youtube.com/watch?v=0JCXmuNnrNc'
-        ),
-        Recording.new(
             'Say Something Sweet to Your Sweetheart',
             'The Ink Spots',
             'https://www.youtube.com/watch?v=z617AUVXyMs'
         ),
         Recording.new(
-            'Suzy Snowflake',
-            'Rosemary Clooney',
-            'https://www.youtube.com/watch?v=UiFXZhU5kp4'
-        ),
-        Recording.new(
-            'The Naughty Lady of Shady Lane',
-            'The Ames Brothers',
-            'https://youtu.be/9HxB7lxbTnI'
-        ),
-        Recording.new(
-            'The Young Ones',
-            'Cliff Richard',
-            'https://www.youtube.com/watch?v=BxNohANhJiA'
+            'Summer Sounds',
+            'Robert Goulet',
+            'https://www.youtube.com/watch?v=1gGJ8AHYloQ'
         ),
         Recording.new(
             'Travelling Light',
@@ -105,10 +125,26 @@ class StaticPagesController < ActionController::Base
             'https://youtu.be/FudzowDyQn0'
         ),
         Recording.new(
-            'Wonderful World of the Young',
-            'Andy Williams',
-            'https://www.youtube.com/watch?v=eoRVnPH8uUI'
-        )
+            'Jenny Kissed Me',
+            'Eddie Albert',
+            'https://youtu.be/Vf-9-rNHjcE'
+        ),
+        Recording.new(
+            'Shoppin Around',
+            'Elvis Presley',
+            'https://youtu.be/ADjm8yzYFW4'
+        ),
+        Recording.new(
+            'Kewpie Doll',
+            'Perry Como',
+            'https://www.youtube.com/watch?v=YJ9W47TIRR4&feature=youtu.be'
+        ),
+        Recording.new(
+            'Santa Claus Looks Just Like Daddy',
+            'Art Mooney & His Orchestra with Barry Gordon',
+            'https://www.youtube.com/watch?v=0JCXmuNnrNc'
+        ),
     ]
   end
 end
+
