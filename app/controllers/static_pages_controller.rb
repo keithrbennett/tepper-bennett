7170cb5ad088c4bf34d7f29387e364583be7e2a3
Keith Bennett <keithrbennett@gmail.com>
2020-05-06 22:46:33 -0400

    Beginnings of Songs table.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ActionController::Base
 
+  Recording = Struct.new(:title, :artist, :url)
+
+  def initialize
+    init_recordings
+  end
+
   def respond
     respond_to do |format|
       format.html
@@ -23,5 +29,26 @@ class StaticPagesController < ActionController::Base
 
   def techhumans
     redirect_to('http://techhumans.com')
+  end
+
+
+  def init_recordings
+    @recordings = [
+        Recording.new(
+            'Red Roses for a Blue Lady',
+            'Bert Kaempfert and His Orchestra',
+            'https://www.youtube.com/watch?v=zt6WdnrAvpE'
+        ),
+        Recording.new(
+            'The Young Ones',
+            'Cliff Richard',
+            'https://www.youtube.com/watch?v=BxNohANhJiA'
+        ),
+        Recording.new(
+            "I've Got a Crush on New York Town",
+            'Connie Francis',
+            'https://www.youtube.com/watch?v=0_Gycn0UJ9M'
+        ),
+    ]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,25 @@ module ApplicationHelper
     tag.a(text.html_safe, href:url, target: '_blank').html_safe
   end
 
+  def external_image_link(image_filename, url)
+    tag.a(href: url, target: '_blank') do
+      image_tag(image_filename, alt: 'YouTube')
+    end
+  end
+
+  def youtube_image_link(url)
+    external_image_link('youtube.png', url)
+  end
+
   def li_external_link(text, url)
     tag.li(external_link(text.html_safe, url)).html_safe
   end
 
+  def song_table_row(recording)
+    tag.tr do
+      tag.td(recording.title) +
+          tag.td(recording.artist) +
+          tag.td(youtube_image_link(recording.url))
+    end.html_safe
+  end
 end
