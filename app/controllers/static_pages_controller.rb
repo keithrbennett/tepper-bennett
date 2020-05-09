06440548642897942b7b13fa1342096df005355a
Keith Bennett <keithrbennett@gmail.com>
2020-05-09 16:14:51 -0400

    Simplify definition of main song rows.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -28,152 +28,39 @@ class StaticPagesController < ActionController::Base
 
 
   def init_songs_pane_recordings
+
+    r = ->(title, artist, code) { Recording.new(title, artist, code) }
+
     @recordings = [
-        Recording.new(
-            'Red Roses for a Blue Lady',
-            'Andy Williams',
-            'HssRO5b_ED0'
-        ),
-        Recording.new(
-            'Red Roses for a Blue Lady',
-            'Bert Kaempfert and His Orchestra',
-            'zt6WdnrAvpE'
-        ),
-        Recording.new(
-            'Red Roses for a Blue Lady',
-            'Dean Martin',
-            'drU6kuih41w'
-        ),
-        Recording.new(
-            'Red Roses for a Blue Lady',
-            'Frank Sinatra',
-            'yeMFw9tBrGo'
-        ),
-        Recording.new(
-            'Red Roses for a Blue Lady',
-            'Guy Lombardo And His Royal Canadians',
-            '_41H2cpTzNc'
-        ),
-        Recording.new(
-            'The Young Ones',
-            'Cliff Richard',
-            'BxNohANhJiA'
-        ),
-        Recording.new(
-            'Kiss of Fire',
-            'Louis Armstrong',
-            'gVxwN3Eaf_U'
-        ),
-        Recording.new(
-            'The Naughty Lady of Shady Lane',
-            'The Ames Brothers',
-            '9HxB7lxbTnI'
-        ),
-        Recording.new(
-            "Nuttin' for Christmas",
-            'Barry Gordon',
-            'aUA7BPnog_0'
-        ),
-        Recording.new(
-            'Suzy Snowflake',
-            'Rosemary Clooney',
-            'UiFXZhU5kp4'
-        ),
-        Recording.new(
-            'Wonderful World of the Young',
-            'Andy Williams',
-            'eoRVnPH8uUI'
-        ),
-        Recording.new(
-            'Bagel & Lox',
-            'Rob Schneider',
-            'dv4h8yU_N7o'
-        ),
-        Recording.new(
-            "Don't Come Running Back to Me",
-            'Nancy Wilson',
-            'QlzCpTEhhQM'
-        ),
-        Recording.new(
-            "I've Got a Crush on New York Town",
-            'Connie Francis',
-            '0_Gycn0UJ9M'
-        ),
-        Recording.new(
-            'Say Something Sweet to Your Sweetheart',
-            'The Ink Spots',
-            'z617AUVXyMs'
-        ),
-        Recording.new(
-            'Summer Sounds',
-            'Robert Goulet',
-            '1gGJ8AHYloQ'
-        ),
-        Recording.new(
-            'Travelling Light',
-            "Herman's Hermits",
-            'stDqoS3zeTE'
-        ),
-        Recording.new(
-            'When the Boy in Your Arms',
-            'Connie Francis',
-            'FudzowDyQn0'
-        ),
-        Recording.new(
-            'Jenny Kissed Me',
-            'Eddie Albert',
-            '/Vf-9-rNHjcE'
-        ),
-        Recording.new(
-            'Shoppin Around',
-            'Elvis Presley',
-            'ADjm8yzYFW4'
-        ),
-        Recording.new(
-            'Angel',
-            'Elvis Presley',
-            '7RQuoPVMPT0'
-        ),
-        Recording.new(
-            'Am I Ready',
-            'Elvis Presley',
-            'E2J13o-RsxA'
-        ),
-        Recording.new(
-            'The Lady Loves Me',
-            'Elvis Presley and Ann-Margret',
-            'Fv0bpfGfzls'
-        ),
-        Recording.new(
-            'Puppet on a String',
-            'Elvis Presley',
-            'RjWoFTu0W28'
-        ),
-        Recording.new(
-            'Kewpie Doll',
-            'Perry Como',
-            'YJ9W47TIRR4'
-        ),
-        Recording.new(
-            'Santa Claus Looks Just Like Daddy',
-            'Art Mooney & His Orchestra with Barry Gordon',
-            '0JCXmuNnrNc'
-        ),
-        Recording.new(
-            "It's a Long Way from Your House to My House",
-            'Frank Sinatra',
-            '7-tcw6w4Cj4'
-        ),
-        Recording.new(
-            'Twenty Tiny Fingers',
-            'The Stargazers',
-            'K0ozZZ_RhP8'
-        ),
-        Recording.new(
-            'Teardrops in the Rain',
-            'Arthur Prysock',
-            '7xVk2GY-TOI'
-        )
+        r.('Red Roses for a Blue Lady',                   'Andy Williams',                        'HssRO5b_ED0'),
+        r.('Red Roses for a Blue Lady',                   'Bert Kaempfert and His Orchestra',     'zt6WdnrAvpE'),
+        r.('Red Roses for a Blue Lady',                   'Dean Martin',                          'drU6kuih41w'),
+        r.('Red Roses for a Blue Lady',                   'Frank Sinatra',                        'yeMFw9tBrGo'),
+        r.('Red Roses for a Blue Lady',                   'Guy Lombardo And His Royal Canadians', '_41H2cpTzNc'),
+        r.('The Young Ones',                              'Cliff Richard',                        'BxNohANhJiA'),
+        r.('Kiss of Fire',                                'Louis Armstrong',                      'gVxwN3Eaf_U'),
+        r.('The Naughty Lady of Shady Lane',              'The Ames Brothers',                    '9HxB7lxbTnI'),
+        r.("Nuttin' for Christmas",                       'Barry Gordon',                         'aUA7BPnog_0'),
+        r.('Suzy Snowflake',                              'Rosemary Clooney',                     'UiFXZhU5kp4'),
+        r.('Wonderful World of the Young',                'Andy Williams',                        'eoRVnPH8uUI'),
+        r.('Bagel & Lox',                                 'Rob Schneider',                        'dv4h8yU_N7o'),
+        r.("Don't Come Running Back to Me",               'Nancy Wilson',                         'QlzCpTEhhQM'),
+        r.("I've Got a Crush on New York Town",           'Connie Francis',                       '0_Gycn0UJ9M'),
+        r.('Say Something Sweet to Your Sweetheart',      'The Ink Spots',                        'z617AUVXyMs'),
+        r.('Summer Sounds',                               'Robert Goulet',                        '1gGJ8AHYloQ'),
+        r.('Travelling Light',                            "Herman's Hermits",                     'stDqoS3zeTE'),
+        r.('When the Boy in Your Arms',                   'Connie Francis',                       'FudzowDyQn0'),
+        r.('Jenny Kissed Me',                             'Eddie Albert',                         '/Vf-9-rNHjcE'),
+        r.('Shoppin Around',                              'Elvis Presley',                        'ADjm8yzYFW4'),
+        r.('Angel',                                       'Elvis Presley',                        '7RQuoPVMPT0'),
+        r.('Am I Ready',                                  'Elvis Presley',                        'E2J13o-RsxA'),
+        r.('The Lady Loves Me',                           'Elvis Presley and Ann-Margret',        'Fv0bpfGfzls'),
+        r.('Puppet on a String',                          'Elvis Presley',                        'RjWoFTu0W28'),
+        r.('Kewpie Doll',                                 'Perry Como',                           'YJ9W47TIRR4'),
+        r.('Santa Claus Looks Just Like Daddy',           'Art Mooney & His Orchestra with Barry Gordon', '0JCXmuNnrNc'),
+        r.("It's a Long Way from Your House to My House", 'Frank Sinatra',                        '7-tcw6w4Cj4'),
+        r.('Twenty Tiny Fingers',                         'The Stargazers',                       'K0ozZZ_RhP8'),
+        r.('Teardrops in the Rain',                       'Arthur Prysock',                       '7xVk2GY-TOI')
     ]
   end
 
