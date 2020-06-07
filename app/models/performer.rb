110a2fb94e5db5da663557ec699bd5f405f94bd5
Keith Bennett <keithrbennett@gmail.com>
2020-06-06 20:51:46 -0400

    Fix typo.

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -3,10 +3,11 @@ class Performer < ApplicationRecord
   has_and_belongs_to_many :songs
   has_and_belongs_to_many :song_plays
 
+  validates_length_of :code, maximum: max_code_length
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  def self.as_report_strings
+  def self.as_report_string
     report = StringIO.new
     report << "          Perfomers\n\n"
     report << '   Code           Name'
