1afa6f5f3d592eb2b6f4200f53a68e0711d7e68b
Keith Bennett <keithrbennett@gmail.com>
2020-06-06 22:05:22 -0400

    Add performer info to song report.

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -3,7 +3,15 @@ class Performer < ApplicationRecord
   has_and_belongs_to_many :songs
   has_and_belongs_to_many :song_plays
 
+
+  def self.max_name_length
+    50
+  end
+
+
   validates_length_of :code, maximum: max_code_length
+  validates_length_of :name, maximum: max_name_length
+
   validates :name, presence: true
   validates :name, uniqueness: true
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -7,22 +7,44 @@ class Song < ApplicationRecord
   has_and_belongs_to_many :rights_admin_orgs, class_name: 'Organization'
   has_many :song_plays
 
+  def self.max_name_length
+    50
+  end
+
   validates_length_of :code, maximum: max_code_length
+  validates_length_of :name, maximum: max_name_length
+
   validates :name, presence: true
   validates :name, uniqueness: true
+
 
   def self.as_report_string
     report = StringIO.new
     report << "          Songs\n\n"
-    report << '   Code           Name'
+    report << '%-*s  %-*s  %-*s  %-*s' %
+        [Song.max_code_length,      '   Code',      Song.max_name_length,      'Name',
+         Performer.max_code_length, 'Perf Code', Performer.max_name_length, 'Performer Name']
     report << "\n\n"
     all.each { |record| report << record.as_report_string << "\n" }
     report.string
   end
 
+
   def as_report_string
-    '%-14s %s' % [code, name]
+
+    @report_string_continuation_indent ||= Song.max_code_length + Song.max_name_length + 4
+
+    sio = StringIO.new
+    sio << '%-*s  %-*s  %-*s  %s' %
+        [Song.max_code_length, code, Song.max_name_length, name,
+         Song.max_code_length, performers.first.code, performers.first.name]
+    performers[1..-1].each do |performer|
+      sio << ("\n%-*s%-*s  %s" %
+          [@report_string_continuation_indent, '', Song.max_code_length, performer.code, performer.name])
+    end
+    sio.string
   end
+
 
 
 end
