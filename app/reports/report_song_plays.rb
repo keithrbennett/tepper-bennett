cf755ce2405b774393210905f33d852d70813d97
Keith Bennett <keithrbennett@gmail.com>
2020-06-14 17:14:58 -0400

    Add JSON & YAML support to song_plays report.

--- app/reports/report_song_plays.rb
+++ app/reports/report_song_plays.rb
@@ -2,30 +2,50 @@ class ReportSongPlays < BaseReport
 
   attr_reader :report_title, :line_length, :report_string_continuation_indent
 
-  YOUTUBE_WATCH_URL_LENGTH = SongPlay::YOUTUBE_WATCH_URL_LENGTH
+  YOUTUBE_KEY_LENGTH = SongPlay::YOUTUBE_KEY_LENGTH
 
   def initialize
-    @line_length = [YOUTUBE_WATCH_URL_LENGTH, Song.max_code_length, Song.max_name_length, Performer.max_code_length, Performer.max_name_length].sum + 8
+    @line_length = [YOUTUBE_KEY_LENGTH, Song.max_code_length, Song.max_name_length, Performer.max_code_length, Performer.max_name_length].sum + 8
     @report_title = 'Song Plays (YouTube)'
     @report_string_continuation_indent = Song.max_code_length + Song.max_name_length + 4
+    build_report_hash(data)
+  end
+
+
+  def data
+    SongPlay.joins(:song).all.order('songs.name, id').map do |song_play|
+      performers = song_play.performers.order(:name).map do |performer|
+        { 'code' => performer.code, 'name' => performer.name }
+      end
+      if performers.count > 1
+        require 'pry'; binding.pry
+      end
+      {
+        'song_code' => song_play.song.code,
+        'song_name' => song_play.song.name,
+        'youtube_key' => song_play.youtube_key,
+        'performers' => performers
+      }
+    end
   end
 
 
   def heading
     '%-*s  %-*s  %-*s  %-*s  %-*s' %
         [
-            YOUTUBE_WATCH_URL_LENGTH, 'YouTube URL',
+            YOUTUBE_KEY_LENGTH,        'YouTube Key',
             Song.max_code_length,      'Song Code',
             Performer.max_code_length, 'Perf Code',
             Song.max_name_length,      'Song Name',
-            Performer.max_name_length, 'Performer Name']
+            Performer.max_name_length, 'Performer Name'
+        ]
   end
 
 
   def report_string
     report = StringIO.new
     report << "#{title_banner}#{heading}\n\n"
-    SongPlay.all.to_a.each { |record| report << record_report_string(record) << "\n" }
+    data.each { |record| report << record_report_string(record) << "\n" }
     report << "\n\n"
     report.string
   end
@@ -39,11 +59,11 @@ class ReportSongPlays < BaseReport
     sio = StringIO.new
     sio << '%-*s  %-*s  %-*s  %-*s  %-*s' %
         [
-            YOUTUBE_WATCH_URL_LENGTH,    record.youtube_watch_url,
-            Song.max_code_length,        record.song.code,
-            Performer.max_code_length,   record.performers[0].code,
-            Song.max_name_length,        record.song.name,
-            Performer.max_name_length,   record.performers[0].name
+            YOUTUBE_KEY_LENGTH,          record['youtube_key'],
+            Song.max_code_length,        record['song_code'],
+            Performer.max_code_length,   record['performers'][0]['code'],
+            Song.max_name_length,        record['song_name'],
+            Performer.max_name_length,   record['performers'][0]['name']
         ]
 
     # performers[1..-1].each do |performer|
