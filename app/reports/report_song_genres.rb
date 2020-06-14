1f75e24bb4246615880b88833c249348515c2990
Keith Bennett <keithrbennett@gmail.com>
2020-06-14 16:23:54 -0400

    Add JSON & YAML support to song_genres report.

--- app/reports/report_song_genres.rb
+++ app/reports/report_song_genres.rb
@@ -5,6 +5,18 @@ class ReportSongGenres < BaseReport
   def initialize
     @line_length = [Song.max_code_length, Song.max_name_length, Performer.max_code_length, Performer.max_name_length].sum + 6
     @report_title = 'Song Genres'
+    build_report_hash(data)
+  end
+
+
+  def data
+    Song.order(:name).all.map do |song|
+      {
+          'code'   => song.code,
+          'name'   => song.name,
+          'genres' => song.genres.pluck(:code)
+      }
+    end
   end
 
 
@@ -19,7 +31,7 @@ class ReportSongGenres < BaseReport
   def report_string
     report = StringIO.new
     report << title_banner << "#{heading}\n\n"
-    Song.all.each { |record| report << record_report_string(record) << "\n" }
+    data.each { |record| report << record_report_string(record) << "\n" }
     report << "\n\n"
     report.string
   end
@@ -27,7 +39,7 @@ class ReportSongGenres < BaseReport
 
   def record_report_string(record)
     '%-*s  %-*s  %s' %
-        [Song.max_code_length, record.code, Song.max_name_length, record.name, record.genre_codes.join('  ')]
+        [Song.max_code_length, record['code'], Song.max_name_length, record['name'], record['genres'].join('  ')]
   end
 
 end
