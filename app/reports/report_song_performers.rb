37c8be23083bb94083b9631e06b6dbca93ff2b73
Keith Bennett <keithrbennett@gmail.com>
2020-06-15 23:20:05 -0400

    Fix song performers JSON and YAML reports.

--- app/reports/report_song_performers.rb
+++ app/reports/report_song_performers.rb
@@ -6,6 +6,18 @@ class ReportSongPerformers < BaseReport
     @line_length = [Song.max_code_length, Song.max_name_length, Performer.max_code_length, Performer.max_name_length].sum + 6
     @report_title = 'Song Performers'
     @report_string_continuation_indent = ' ' * (Song.max_code_length + Song.max_name_length + 4)
+    build_report_hash(data)
+  end
+
+
+  def data
+    @data ||= Song.order(:name).map do |song|
+      performers = song.performers.map do |performer|
+        { 'code' => performer.code, 'name' => performer.name }
+      end
+
+      { 'code' => song.code, 'name' => song.name, 'performers' => performers}
+    end
   end
 
 
@@ -21,7 +33,7 @@ class ReportSongPerformers < BaseReport
   def report_string
     report = StringIO.new
     report << "#{title_banner}#{heading}\n\n"
-    Song.all.each { |record| report << record_report_string(record) << "\n" }
+    data.each { |record| report << record_report_string(record) << "\n" }
     report << "\n\n"
     report.string
   end
@@ -32,15 +44,15 @@ class ReportSongPerformers < BaseReport
     # and Stack Overflow page:
     # https://stackoverflow.com/questions/62273455/activerecord-associationrelation-weird-first-last-behavior
 
-    performers = record.performers.order(:name).to_a
+    performers = record['performers']
     sio = StringIO.new
     sio << '%-*s  %-*s  %-*s  %s' %
-        [Song.max_code_length, record.code, Song.max_name_length, record.name,
-         Song.max_code_length, performers&.first&.code, performers&.first&.name]
+        [Song.max_code_length, record['code'], Song.max_name_length, record['name'],
+         Song.max_code_length, performers&.first&.[]('code'), performers&.first&.[]('name')]
 
     (performers[1..-1] || []).each do |performer|
       sio << "\n%s%-*s  %s" %
-          [@report_string_continuation_indent, Song.max_code_length, performer.code, performer.name]
+          [@report_string_continuation_indent, Song.max_code_length, performer['code'], performer['name']]
     end
     sio.string
   end
