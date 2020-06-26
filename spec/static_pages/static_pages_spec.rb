5eb7d0295c6a8d5f6029ff88de750bed6105b044
Keith Bennett <keithrbennett@gmail.com>
2020-06-26 00:40:54 -0400

    Minor fix

--- spec/static_pages/static_pages_spec.rb
+++ spec/static_pages/static_pages_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 # To save and open a screenshot, add `, js: true` to the `describe` call like below, and call `save_and_open_page`
 RSpec.describe 'visit the home page' do
 # RSpec.describe 'visit the home page', js: true do
-  specify js: true do
+  specify do
     visit '/'
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_selector('.title')
     expect(page).to have_content('Bennett')
     expect(page).to have_content('Tepper')
