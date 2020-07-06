2215249713fa9c48f995a556b2d56fcf56fde31e
Keith Bennett <keithrbennett@gmail.com>
2020-07-06 17:00:10 -0400

    Implement song performers text report.

--- app/lib/reports/song_performers_report.rb
+++ app/lib/reports/song_performers_report.rb
@@ -10,19 +10,25 @@ class SongPerformersReport < BaseReport
           performers: pluck_to_hash(song.performers.order(:name), :code, :name)
       }
     end
+  end
 
-    def to_html
-      headings = ['Song Code', 'Song Name', 'Perf Code', 'Performer Name']
-      data = records.map do |r|
-        [
-            r[:code],
-            r[:name],
-            r[:performers].pluck(:code).join("<br/>"),
-            r[:performers].pluck(:name).join("<br/>")
-        ]
-      end
-      table_data = records_to_html_table_data(data)
-      html_report_table(headings, table_data)
+
+  def to_html
+    headings = ['Song Code', 'Song Name', 'Perf Code', 'Performer Name']
+    data = records.map do |r|
+      [
+          r[:code],
+          r[:name],
+          r[:performers].pluck(:code).join("<br/>"),
+          r[:performers].pluck(:name).join("<br/>")
+      ]
     end
+    table_data = records_to_html_table_data(data)
+    html_report_table(headings, table_data)
+  end
+
+
+  def to_raw_text
+    SongPerformersTextReport.new(records).report_string
   end
 end
--- app/lib/reports/song_performers_text_report.rb
+++ app/lib/reports/song_performers_text_report.rb
@@ -2,40 +2,34 @@ require_relative 'base_text_report'
 
 class SongPerformersTextReport < BaseTextReport
 
-  attr_reader :title, :line_length, :report_string_continuation_indent
+  attr_reader :records, :title, :line_length
 
-  def initialize
+  def initialize(records)
+    @records = records
     @line_length = [Song.max_code_length, Song.max_name_length, Performer.max_code_length, Performer.max_name_length].sum + 6
-    @report_title = 'Song Performers'
-    @report_string_continuation_indent = ' ' * (Song.max_code_length + Song.max_name_length + 4)
-    build_report_hash(data)
+    @title = 'Song Performers'
+    build_report_hash(records)
   end
 
 
-  def data
-    @data ||= Song.order(:name).map do |song|
-      performers = song.performers.map do |performer|
-        { 'code' => performer.code, 'name' => performer.name }
-      end
-
-      { 'code' => song.code, 'name' => song.name, 'performers' => performers}
-    end
+  def formatted_data_line(song_code, song_name, perf_code, perf_name)
+    '%-*s  %-*s  %-*s  %-*s' %
+        [Song.max_code_length,      song_code,
+         Song.max_name_length,      song_name,
+         Performer.max_code_length, perf_code,
+         Performer.max_name_length, perf_name]
   end
 
 
   def heading
-    '%-*s  %-*s  %-*s  %-*s' %
-        [Song.max_code_length,      '   Code',
-         Song.max_name_length,      'Name',
-         Performer.max_code_length, 'Perf Code',
-         Performer.max_name_length, 'Performer Name']
+    formatted_data_line('   Code', 'Name', 'Perf Code', 'Performer Name')
   end
 
 
   def report_string
     report = StringIO.new
     report << "#{title_banner}#{heading}\n\n"
-    data.each { |record| report << record_report_string(record) << "\n" }
+    records.each { |record| report << record_report_string(record) << "\n" }
     report << "\n\n"
     report.string
   end
@@ -46,15 +40,12 @@ class SongPerformersTextReport < BaseTextReport
     # and Stack Overflow page:
     # https://stackoverflow.com/questions/62273455/activerecord-associationrelation-weird-first-last-behavior
 
-    performers = record['performers']
+    performers = record[:performers]
     sio = StringIO.new
-    sio << '%-*s  %-*s  %-*s  %s' %
-        [Song.max_code_length, record['code'], Song.max_name_length, record['name'],
-         Song.max_code_length, performers&.first&.[]('code'), performers&.first&.[]('name')]
+    sio << formatted_data_line(record[:code], record[:name], performers[0][:code], performers[0][:name])
 
     (performers[1..-1] || []).each do |performer|
-      sio << "\n%s%-*s  %s" %
-          [@report_string_continuation_indent, Song.max_code_length, performer['code'], performer['name']]
+      sio << "\n" + formatted_data_line('', '', performer[:code], performer[:name])
     end
     sio.string
   end
