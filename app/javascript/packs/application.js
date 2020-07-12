ae3de1677963cc1b3be8b22d8d9ece81081413f0
kbennett <keithrbennett@gmail.com>
2020-07-12 17:45:27 -0400

    Revert "Add FontAwesome to project." It seems that it was already included in the Bootstrap distribution. This reverts commit d419e47015124ec134884986b19f9396c796e3f1.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 
 import "bootstrap"
 import "../stylesheets/application"
-import "@fortawesome/fontawesome-free/js/all";
 
 function defaultBackgroundColor() {
     return "#c9d0f1";
