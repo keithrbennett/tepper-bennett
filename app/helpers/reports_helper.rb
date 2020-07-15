c1c771195c768bd84362ed9c5a59be8f8340231a
kbennett <keithrbennett@gmail.com>
2020-07-15 16:25:44 -0400

    Cleanup.

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,6 +1,6 @@
 module ReportsHelper
 
-  class ReportMetadata < Struct.new(:rpt_type, :title, :fn_report) #, :report_text, :report_json, :report_yaml)
+  class ReportMetadata < Struct.new(:rpt_type, :title, :fn_report)
 
     include ApplicationHelper
 
@@ -18,37 +18,9 @@ module ReportsHelper
           card_button_id:  button_id,
           card_content_id: content_id,
           title:    title,
-          # fn_url:          ->(format) { "/reports?type=#{content_id}&format=#{format}" },
-          # reporters:       {
-          #     html: fn_html_report,
-          #     text: -> { report_text },
-          #     json: -> { report_json },
-          #     yaml: -> { report_yaml },
-          # },
           copy_button_id:  "btn-copy-#{rpt_type}",
           content_tab_id:  "content-tab-#{rpt_type}"
       }
     end
-
-    # def report_filespec(extension)
-    #   File.join(Rails.root, 'app', 'generated_reports', "#{rpt_type}_report#{extension}")
-    # end
-    #
-
-    # def preize_file_content(extension)
-    #   "<div><pre>\n".html_safe + File.read(report_filespec(extension)) + "</pre></div>\n".html_safe
-    # end
-
-    # def report_text
-    #   @report_text ||= preize_file_content('.txt')
-    # end
-    #
-    # def report_json
-    #   @report_json ||= preize_file_content('.json')
-    # end
-    #
-    # def report_yaml
-    #   @report_yaml ||= preize_file_content('.yaml')
-    # end
   end
 end
