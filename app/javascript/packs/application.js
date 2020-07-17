2eaf936c8903840fadca2cd3d4fdf9d1c02f68ad
Keith Bennett <keithrbennett@gmail.com>
2020-07-17 00:09:48 -0400

    Extract reports-specific JS to reports.js.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("packs/reports")
 
 import "bootstrap"
 import "../stylesheets/application"
@@ -94,80 +95,20 @@ function setUpColorPicker() {
 }
 
 
-function setUpReportCopyButtons() {
-
-    const button = document.querySelector("#rpt-copy-button");
-    if (! button)
-        return;
-
-    const handler = function() {
-        const textToCopy = new RegExp("<div><pre>(.*)</pre></div>").
-                exec(activePane.innerHTML)[1]
-
-        const activePane = document.querySelector(".rpt-tab-pane.active");
-        activePane.focus(); // without this, the clipboard copy fails
-
-        navigator.clipboard.writeText(textToCopy)
-        .then(
-            () => {
-                console.log("Copied to clipboard: " + activePane.id);
-                alert("Content copied to clipboard.");
-            },
-            (error) => { console.log("Error copying text from", activePane.id + ':', error); }
-        )
-    }
-
-    button.addEventListener("click", handler);
-}
-
-function setupCopyButtonVisibility() {
-    // Nothing to do on the button itself, but the tabs need to be set up to control its visibility.
-
-    const copyButton = document.querySelector("#rpt-copy-button");
-    if (! copyButton)
-        return;
-
-    copyButton.style.visibility = "hidden"; // hidden because initial tab selected is HTML
-
-    const buttonVisibilityHandler = function (visible) {
-        return function() {
-            copyButton.style.visibility = visible ? "visible" : "hidden";
-        }
-    }
-
-    for (const rptTab of document.querySelectorAll(".rpt-nav-tab")) {
-        const copyButtonVisible = ! rptTab.id.includes("html");
-        rptTab.addEventListener("click", buttonVisibilityHandler(copyButtonVisible));
-    }
-}
-
-
-function setupReportBackButton() {
-    const button = document.querySelector("#rpt-back-button");
-    if (! button) // this will be called on all pages, some without the button
-        return;
-
-    const loadReportsPage = function() { window.location.href = "/reports"; };
-    button.addEventListener("click", loadReportsPage);
-}
-
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log("DOM content loaded.");
+    console.log("DOM content loaded, initializing application.");
     setUpYouTubeClicks();
 });
 
 
 document.addEventListener("turbolinks:load", () => {
-    console.log("Turbolinks loaded.");
+    console.log("Turbolinks loaded, initializing application.");
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
     setUpMainMenuLinks();
     setInitialMenuChoice();
     setUpColorPicker();
-    setUpReportCopyButtons();
-    setupCopyButtonVisibility();
-    setupReportBackButton();
 });
 
 
---
+++ app/javascript/packs/reports.js
@@ -0,0 +1,70 @@
+
+
+function setUpReportCopyButtons() {
+
+    const button = document.querySelector("#rpt-copy-button");
+    if (! button)
+        return;
+
+    const handler = function() {
+        const textToCopy = new RegExp("<div><pre>(.*)</pre></div>").
+        exec(activePane.innerHTML)[1]
+
+        const activePane = document.querySelector(".rpt-tab-pane.active");
+        activePane.focus(); // without this, the clipboard copy fails
+
+        navigator.clipboard.writeText(textToCopy)
+            .then(
+                () => {
+                    console.log("Copied to clipboard: " + activePane.id);
+                    alert("Content copied to clipboard.");
+                },
+                (error) => { console.log("Error copying text from", activePane.id + ':', error); }
+            )
+    }
+
+    button.addEventListener("click", handler);
+}
+
+function setupCopyButtonVisibility() {
+    // Nothing to do on the button itself, but the tabs need to be set up to control its visibility.
+
+    const copyButton = document.querySelector("#rpt-copy-button");
+    if (! copyButton)
+        return;
+
+    copyButton.style.visibility = "hidden"; // hidden because initial tab selected is HTML
+
+    const buttonVisibilityHandler = function (visible) {
+        return function() {
+            copyButton.style.visibility = visible ? "visible" : "hidden";
+        }
+    }
+
+    for (const rptTab of document.querySelectorAll(".rpt-nav-tab")) {
+        const copyButtonVisible = ! rptTab.id.includes("html");
+        rptTab.addEventListener("click", buttonVisibilityHandler(copyButtonVisible));
+    }
+}
+
+
+function setupReportBackButton() {
+    const button = document.querySelector("#rpt-back-button");
+    if (! button) // this will be called on all pages, some without the button
+        return;
+
+    const loadReportsPage = function() { window.location.href = "/reports"; };
+    button.addEventListener("click", loadReportsPage);
+}
+
+
+
+
+document.addEventListener("turbolinks:load", () => {
+    console.log("Turbolinks loaded, initializing reports.");
+    setUpReportCopyButtons();
+    setupCopyButtonVisibility();
+    setupReportBackButton();
+});
+
+
