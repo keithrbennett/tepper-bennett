41b5bef879d70d6a946a607e06483f5bfeec6237
Keith Bennett <keithrbennett@gmail.com>
2020-08-03 16:59:58 -0400

    Center song title on song page. Remove unused code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,13 +10,6 @@ module ApplicationHelper
   end
 
 
-  def validate_artist_or_movie(artist_or_movie)
-    unless %i(artist movie).include?(artist_or_movie)
-      raise "Invalid symbol: #{artist_or_movie}. Must be :artist or :movie."
-    end
-  end
-
-
   def inspect(object)
     object.ai(html: true, plain:true, multiline: true)
   end
