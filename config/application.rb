1f1aa578aadaa59b4e9df3bbc45816a4307dec3c
Keith Bennett <keithrbennett@gmail.com>
2024-03-24 15:00:23 +0700

    In application.rb, updated defaults from 6 to 7, and added `config.autoloader = :classic` to disable zeitwerk (hopefully temporarily).

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module TepperBennett
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
+    config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
