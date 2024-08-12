5863a3142633e402edb49d281abc7ed065800bbf
Keith Bennett <keithrbennett@gmail.com>
2024-08-12 22:49:08 +0800

    Remove unused method.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,21 +17,6 @@ Turbo.start();
 import $ from 'jquery';
 window.$ = $;
 
-function rgbToHex(r, g, b) {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
-
-    if (r.length == 1)
-        r = "0" + r;
-    if (g.length == 1)
-        g = "0" + g;
-    if (b.length == 1)
-        b = "0" + b;
-
-    return "#" + r + g + b;
-}
-
 const initialize_application = function() {
   try {
     function defaultBackgroundColor() {
