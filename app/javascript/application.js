fbb3e16d56e5e89f01cd49deeee7c61ee66e96d7
Keith Bennett <keithrbennett@gmail.com>
2024-08-21 18:47:00 +0800

    Fix the way Bootstrap JS is loaded.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
-import 'bootstrap'
+import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'datatables.net-bs5'
 import DataTable from 'datatables.net-bs5';
 
